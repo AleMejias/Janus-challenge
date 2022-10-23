a6e145a8c77f4e588f2e992c12ea0122f64caa81
AleMejias <alejandro_12_77@hotmail.com>
2022-10-23 16:53:49 -0300

    Avances de metodos del backend para traer datos

---
+++ backend_productos/backend_productos/Models/ManejadorDeProductos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace backend_productos.Models
+{
+    public class ManejadorDeProductos
+    {
+        public List<Producto> getTodos()
+        {
+            List<Producto> listaProductos = new List<Producto>();
+            string url = ConfigurationManager.ConnectionStrings["Test"].ToString();
+
+            using (SqlConnection con = new SqlConnection(url))
+            {
+                con.Open();
+
+                SqlCommand Command = con.CreateCommand();
+                Command.CommandText = "sp_ObtenerProductos";
+                Command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while( Reader.Read() )
+                {
+                    int id = Reader.GetInt32(0);
+                    int idTipoProducto = Reader.GetInt32(1);
+                    string nombre = Reader.GetString(2).Trim();
+                    int precio = Reader.GetInt32(3);
+                    int activo = Reader.GetInt32(4);
+                    int stock = Reader.GetInt32(5);
+
+
+                    Producto producto = new Producto( id , idTipoProducto , nombre , precio , activo , stock );
+
+                    listaProductos.Add(producto);
+                }
+
+                Reader.Close();
+                con.Close();
+            }
+
+            return listaProductos;
+        }
+    }
+}
---
+++ backend_productos/backend_productos/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend_productos.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public int idTipoProducto { get; set; }
+        public string nombre { get; set; }
+        public int precio { get; set; }
+        public int activo  { get; set; }
+        public int stock { get; set; }
+
+
+        public Producto()
+        {
+
+        }
+        public Producto( int id , int idTipoProducto , string nombre , int precio , int activo , int stock )
+        {
+
+        }
+    }
+}
